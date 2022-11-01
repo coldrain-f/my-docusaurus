ea43f131ffcdc9fea633460fb5b84b12f59395c2
coldrain-f <intmain__@naver.com>
2022-11-01 19:57:50 +0900

    update: code block - java, powershell, rust 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -261,6 +261,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['powershell', 'rust', 'java'],
       },
     }),
 };
